5fb34135acddfe0fb81bdead2aa46841cbd5a46d
Gabriele <gabrocecco@gmail.com>
2024-11-30 17:25:46 +0100

    omp new version

--- omp.c
+++ omp.c
@@ -1,168 +1,167 @@
 /****************************************************************************
  * Compile with:
- * gcc -fopenmp omp.c -o omp
+ * gcc -fopenmp omp.c -o omp -lm 
  *
  * Run with:
- * OMP_NUM_THREADS=4 ./omp N K 
+ * ./omp N R K 
  *
  ****************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include <omp.h>
 #include "hpc.h"
+#include <sys/resource.h>
 
-/* Global constants */
-int R = 3; /* Kernel lenght, costant for all layers */
+const float bias = 0.1; // Constant bias 
 
+
+// Sgimoid, simple version 
 float sigmoid(float x) {
-    // return 1.0 / (1.0 + exp(-x));
-    return x / (1 + abs(x));
+    return 1.0 / (1.0 + exp(-x));
 }
 
-/* Fills n x n square matrix m with random values */
-void fill( float* m, int n )
+// Utility function for extarct peak memory usage
+void print_memory_usage() {
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    printf("Peak memory usage: %.2f MB\n", usage.ru_maxrss / 1024.0);
+}
+
+// Forward funcion for calclulate next layer output
+void compute_layer( float *input,   // pointer to the array of the input layer 
+                    float *output,  // pointer to the array of the output layer, for storing the results 
+                    float *weights, // pointe to the global array of weights 
+                    float bias, // bias, constant for all computations 
+                    int N,  // number of neurons in output layer
+                    int R,  // constant R 
+                    int offset  // offset for accessing weights array for the current layer 
+                ) 
 {
-    int i, j;
-    for (i=0; i<n; i++) {
-        m[i] = (float)rand() / RAND_MAX;
-    }
-}
-
-void fill_zeros( float* m, int n ){
-    int i, j;
-// #pragma omp parallel for
-    for (i=0; i<n; i++) {
-        m[i] = (float)0;
-    }
-}
-
-void forward_propagation(int K, int R, int* layers_neuron_number, float* V, float* W, float* B) {
-    int new_value_index = layers_neuron_number[0];  /* Start from the first neuron of layer 1 */
-    int old_value_index = 0;  /* Start from the first neuron of the input layer (layer 0) */
-    int weight_index = 0;     /* All weights are stored contiguously */
-
-    for (int k = 1; k < K; k++) { // Non <= K
-
-#pragma omp parallel for schedule(static)
-        for (int i = 0; i < layers_neuron_number[k]; i++) { /* For every output neutron y_i*/
-            float sum = 0.0;
-            for (int r = 0; r < R; r++) {   /* Iterate on R last layer neutrons for calculate the sum  */
-                int weight_idx = weight_index + (i * R) + r;    /* Local layer index of weights, for process*/ 
-                int input_index = old_value_index + i + r;
-                sum += V[input_index] * W[weight_index];
-            }
-            sum += B[k - 1];
-            V[new_value_index + i] = sigmoid(sum);
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {   // for every output neuron
+        float sum = 0.0;
+        for (int r = 0; r < R; r++) {   // R computations with R differente weights for each output neuron 
+            sum += input[i + r] * weights[offset + i * R + r];
         }
-        
-        weight_index += layers_neuron_number[k] * R;
-        old_value_index = new_value_index;
-        new_value_index += layers_neuron_number[k];
+        output[i] = sigmoid(sum + bias);
     }
 }
 
-int main( int argc, char *argv[] )
-{
+int main(int argc, char *argv[]) {
     float tstart, tstop;
-    /* Initialized random generator */
-    srand(time(NULL));
-    /* Constants passed as argoument*/
-    int N;  /* N = Number of neurons on layer 0 */
-    int K;  /* K = Number of layers in the network */
-    int N_neurons; /* Numbers of neurons from leyer 1 to layer K-1 */
-    float *I, *W, *B, *V;  /* Data */
-    int *layers_neuron_number;  /* Array with the numbers of neuron for each layer*/
-    
-    if (argc == 3) {
-        N = atoi(argv[1]);
-        K = atoi(argv[2]);
-    }
-    else return -1;
-    
-    printf("N = %d K = %d R = %d\n", N, K, R);
+    long tot_number_of_bytes_allocated = 0;
 
-    omp_set_num_threads(atoi(getenv("OMP_NUM_THREADS"))); // Usa il numero di thread specificato
-    printf("P = %s\n", getenv("OMP_NUM_THREADS"));
-    /* Compute the total amount of neurons from input layer
-        to K-1 layer.
-    */
-    layers_neuron_number = (int*)malloc( K * sizeof(int) );  
-    N_neurons = 0;
-    for(int t = 0; t < K; t++){
-        N_neurons += (N - t*(R - 1));
-        layers_neuron_number[t] = (N - t*(R - 1));
-    }
-    printf("Total neurons: %d \n", N_neurons);    
-
-    /* Data generation with random float values, we need:
-        - Layer 0: N input values
-        - For each neuron we need 3 weights
-        - In total we have N_neurons * 3 weights
-        - For each layer we have 1 bias value 
-         */
-    // I = (float*)malloc( N * sizeof(float) );  /* Input layer vector */
-    W = (float*)malloc( N_neurons * R * sizeof(float) ); /* Weights vector */
-    B = (float*)malloc( (K - 1) * sizeof(float) ); /* Bias vector */
-    V = (float*)malloc( (N + N_neurons) * sizeof(float) ); /* Neurons value vector  */
-
-    if (V == NULL || W == NULL || B == NULL) {
-        printf("Memory allocation failed.\n");
+    if (argc != 4) {
+        printf("Usage: %s <N> <R> <K>\n", argv[0]);
         return -1;
     }
 
-    fill_zeros(V, N_neurons); // hidden layers has value 0 at the start
-    fill(V, N); // we allocate the first N neurons value for the input layer 
+    /* Reading input parameters */
+    int N = atoi(argv[1]); 
+    int R = atoi(argv[2]); 
+    int K = atoi(argv[3]); 
+    printf("N=%d, R=%d, K=%d\n", N, R, K);
+    
+    // Computation of total number of weights 
+    int total_weights = 0;
+    for (int t = 0; t < K - 1; t++) {   // for K-1 layers (no weights for input layer)
+        int layer_size = N - t * (R - 1);   // number of weights in the current layer
+        total_weights += layer_size * R;    // we have R unique weights for each neuron 
+    }
+    printf("Nummber of weights: %d\n", total_weights);
+    
+    //--------------------------------------------------------DATA PREPARATION --------------------------------------------------------------
+    printf("\n-----------------DATA PREPARATION-----------------\n");
     tstart = hpc_gettime();
-    fill(W, N_neurons * 3);
-    tstop = hpc_gettime();
-    printf("Data preparation execution time %f\n", tstop - tstart); 
+    // Data allocation 
+    float **layers = (float **)malloc(K * sizeof(float *)); // we use K arrays for the layer value, one for each layer
+    float *weights = (float *)malloc(total_weights * sizeof(float));    // we use a single large array for storing weights sequentially
+    printf("Allocating %ld bytes (%ld MB) of weights.\n", (total_weights * sizeof(float)), (total_weights * sizeof(float) / 1000000));
 
-    fill(B, K - 1);
+    tot_number_of_bytes_allocated += (K * sizeof(float *)) + (total_weights * sizeof(float));
 
-    // printf("\nINPUT_VECTOR (I), size %d\n ", N);
-    // printf("\nWEIGHTS_VECTOR (W), size %d\n", N_neurons * 3);
-    // printf("\nBIAS_VECTOR (B), size %d\n", K-1);
-    // printf("\n\n");
-
-    float oneCoreReference = 0.0;
-    float speedup = 1;
-    float lastspeedup = 1;
-    float difference = 0;
-    float best = 1;
-    int bestP = 1;
-    float bestDiff = 0;
-    for(int p=1; p<=30; p++){
-        omp_set_num_threads(p);
-        printf("P=%d\n", p);
-        tstart = hpc_gettime();
-        forward_propagation(K, R, layers_neuron_number, V, W, B);
-        for(int i = N_neurons - 10; i < N_neurons; i++){
-            printf("%f ", V[i]);
-        }
-        tstop = hpc_gettime();
-        if(p==1){
-            oneCoreReference = tstop - tstart;
-            printf("Time: %f REFERENCE %f\n", tstop - tstart, speedup); 
-        } else{
-            lastspeedup = speedup;
-            speedup = (oneCoreReference / (tstop - tstart)) * 100;
-            difference = speedup - best;
-            if(speedup > best){
-                best = speedup;
-                bestP = p;
-                bestDiff = difference;
+    /* Allocation of layers values and initialization of input layer */
+    long number_of_values = 0;
+    for (int t = 0; t < K; t++) {
+        int layer_size = N - t * (R - 1);
+        layers[t] = (float *)malloc(layer_size * sizeof(float));    // data allocation of values for each of K layers
+        tot_number_of_bytes_allocated += (layer_size * sizeof(float));
+        // We initailize only the input layer
+        if (t == 0) {
+            for (int i = 0; i < layer_size; i++) {
+                layers[0][i] = ((float)rand() / RAND_MAX);
             }
-            printf("Time: %f +%.2f%% (diff from best: +%.2f%%) \n", tstop - tstart, speedup, difference); 
         }
     }
-    printf("\nBest speedup: +%.2f%% (diff from best: +%.2f%%), P=%d\n", best, bestDiff,bestP);
+
+    // Weigths initialization 
+    for (int i = 0; i < total_weights; i++) {
+        weights[i] = ((float)rand() / RAND_MAX);
+    }
+    tstop = hpc_gettime();
+    printf("Data preparation time: %f\n", tstop - tstart);
+    printf("Total number of bytes allocated: %ld (%ld MB)\n",tot_number_of_bytes_allocated, tot_number_of_bytes_allocated/1000000);
 
 
-    free(layers_neuron_number);
-    free(V);
-    free(W);
-    free(B);
+    /* ----------------COMPUTATION---------------------------------------------------------------------------------------------*/
+    printf("\n-----------------COMPUTUTATION-----------------\n");
+    float serial_time;
+    float best_time;
+    int best_p = 1;
+    float best_speedup;
+    float current_time;
+    for(int p=1; p <= 20; p=p*2){
+        omp_set_num_threads(p);
+        tstart = hpc_gettime();   
+        int offset = 0; // Offset per accedere ai pesi del livello corrente
+        for (int t = 0; t < K - 1; t++) {   // for K-1 layers 
+            int current_layer_size = N - t * (R - 1);
+            int next_layer_size = N - (t + 1) * (R - 1);
+
+            compute_layer(layers[t], layers[t + 1], weights, bias, next_layer_size, R, offset);
+
+            // Aggiorna l'offset per i pesi
+            offset += current_layer_size * R;
+        }
+        tstop = hpc_gettime();   
+        // printf("Inference time: %f\n", tstop - tstart);
+        current_time = tstop - tstart;
+        if(p==1){
+            serial_time = current_time;
+            best_time = current_time;
+            best_speedup = 1;
+        }
+        if(best_time > current_time){
+            best_time = tstop - tstart;
+            best_p = p;
+            best_speedup = (serial_time)/(tstop - tstart);
+        }
+        printf("P=%d, time: %.4f, speedup: %.3f, \n", p, tstop - tstart, (serial_time)/(tstop - tstart));
+
+        // Printing first and last 10 values of output layer 
+        // printf("Output Layer (first and last 10\n");
+        // for (int i = 0; i < 10; i++) {
+        //     printf("%.4f ", layers[K - 1][i]);
+        // }
+        // printf("\n ...\n");
+        // for (int i = N - (K - 1) * (R - 1) - 10; i < N - (K - 1) * (R - 1); i++) {
+        //     printf("%.4f ", layers[K - 1][i]);
+        // }
+        printf("\n");
+    }
+    printf("P=%d best time %.4f: best speedup: %.3f\n", best_p, best_time, best_speedup);
+
+    // Printf peak memory usage
+    print_memory_usage();
+
+    // Memory deallocation
+    for (int t = 0; t < K; t++) {
+        free(layers[t]);
+    }
+    free(layers);
+    free(weights);
+
     return 0;
 }
+
